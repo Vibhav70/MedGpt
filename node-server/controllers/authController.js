3a7e3f851038a9fc05983c4d3df73221bc98a400
Aaditya Pandey <aprock3004@gmail.com>
2025-01-17 03:18:15 +0530

    Authentication complete

--- node-server/controllers/authController.js
+++ node-server/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const generateCustomerId = require("../utils/generateId");
 
+// Login function
 const loginUser = async (req, res) => {
   const { username, password } = req.body;
   try {
@@ -15,4 +16,27 @@ const loginUser = async (req, res) => {
   }
 };
 
-module.exports = { loginUser };
+// Signup function
+const signupUser = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    // Check if the user already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+
+    // Generate a unique customer ID and create a new user
+    const customer_id = generateCustomerId();
+    const newUser = await User.create({ username, password, customer_id });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      customer_id: newUser.customer_id,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Signup failed", error: err.message });
+  }
+};
+
+module.exports = { loginUser, signupUser };
